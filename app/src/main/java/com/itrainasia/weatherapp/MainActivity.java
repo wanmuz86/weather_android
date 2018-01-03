77e562de480995359dabbcf8dc7290166e94ca14
wan muzaffar Wan Hashim <wanmuz86@gmail.com>
2018-01-03 20:00:33 +0800

    add volley

--- app/src/main/java/com/itrainasia/weatherapp/MainActivity.java
+++ app/src/main/java/com/itrainasia/weatherapp/MainActivity.java
@@ -2,6 +2,14 @@ package com.itrainasia.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&appid=9fd7a449d055dba26a982a3220f32aa2";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("debug", response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("error",error.toString());
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
